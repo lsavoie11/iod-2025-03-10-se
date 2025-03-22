2603c273491189a133414d6b0bee7c9ef4f9ae61
lsavoie11 <lsavoie11@gmail.com>
2025-03-22 13:00:47 -0500

    Lab Exercise 5

---
+++ module-1/labExercises/labExercise5/Lab5.js
@@ -0,0 +1,6 @@
+let pLanguages = ["JavaScript", "Python", "Java", "C#", "C++"];
+pLanguages[1] = "Ruby";
+pLanguages[4] = "PHP";
+pLanguages.unshift("CSS");
+pLanguages.pop();
+console.log(pLanguages);
