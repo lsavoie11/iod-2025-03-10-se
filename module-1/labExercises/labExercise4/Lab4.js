b09e1cd55b975e5e9d358152835527f39bf17db9
lsavoie11 <lsavoie11@gmail.com>
2025-03-22 08:47:29 -0500

    Lab Exercise 4

---
+++ module-1/labExercises/labExercise4/Lab4.js
@@ -0,0 +1,24 @@
+function func1(num1, num2) {
+  return num1 + num2;
+}
+console.log(func1(2, 3));
+
+function func2(num1, num2) {
+  return num1 - num2;
+}
+console.log(func2(2, 5));
+
+function func3(num1, num2) {
+  return num1 * num2;
+}
+console.log(func3(4, 6));
+
+function func4(num1, num2) {
+  return num1 / num2;
+}
+console.log(func4(8, 2));
+
+function moniker(name) {
+  return "Hello " + name;
+}
+console.log(moniker("Joey"));
