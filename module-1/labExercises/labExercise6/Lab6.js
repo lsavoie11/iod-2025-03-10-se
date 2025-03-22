c4bc34c5d6edb72bd28596f8f73b27a6f1b26673
lsavoie11 <lsavoie11@gmail.com>
2025-03-22 13:08:16 -0500

    Lab Exercise 6

---
+++ module-1/labExercises/labExercise6/Lab6.js
@@ -0,0 +1,41 @@
+const book = {
+  title: "The Second Sex",
+  author: "Simone de Beauvoir",
+  year: 1949,
+  pages: 768,
+};
+
+book.pages = 800;
+book.year = 1953;
+
+const book2 = {
+  title: "We: Understanding the Psychology of Romantic Love",
+  author: "Robert A. Johnson",
+  year: 1983,
+  pages: 224,
+};
+
+const book3 = {
+  title: "Chop Wood Carry Water",
+  author: "Rick Fields, Peggy Taylor, Rex Weyler, Rick Ingrasci",
+  year: 1984,
+  pages: 304,
+};
+
+const book4 = {
+  title: "Tao Te Ching",
+  author: "Lao Tzu",
+  year: 351,
+  pages: 107,
+};
+
+const book5 = {
+  title: "The Power of Now",
+  author: "Eckhart Tolle",
+  year: 1997,
+  pages: 236,
+};
+
+let books = [book, book2, book3, book4, book5];
+
+console.log(books);
