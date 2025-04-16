f85d50fd2ae9fc8349b3e87174e700b189192007
lsavoie11 <lsavoie11@gmail.com>
2025-04-16 11:12:51 -0500

    3.3 commit

---
+++ module-3/Fundamentals_3.3.js
@@ -0,0 +1,10 @@
+//The value of 0 is falsy, so the if statement will not execute the console.log() statement.
+if (0) console.log("#1 zero is true");
+
+if ("0") console.log("#2 zero is true");
+// The value of null is falsy, so the if statement will not execute the console.log() statement.
+if (null) console.log("null is true");
+
+if (-1) console.log("negative is true");
+
+if (1) console.log("positive is true");
