68cf5547b84652bff743b88c7dbc0bea92ab7588
lsavoie11 <lsavoie11@gmail.com>
2025-04-21 10:24:24 -0500

    remove

--- module-3/JS_Intermediate_1.js
+++
@@ -1,8 +0,0 @@
-function capitalizeWords(input) {
-  return input
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
-
-console.log(capitalizeWords("hello world"));
