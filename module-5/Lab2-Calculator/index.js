d9640c87846dbaf14c9501cde1d7d0fa646cd038
lsavoie11 <lsavoie11@gmail.com>
2025-05-31 12:09:00 -0500

    Backend of Calculator for Lab Exercise 2

---
+++ module-5/Lab2-Calculator/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// import all calculator routes (up the top)
+const calculatorRoutes = require("./routes/calculatorRoutes");
+
+// map the calculator routes to our app
+app.use("/calculator", calculatorRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
