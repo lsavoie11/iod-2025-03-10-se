a0197c8d69c5bbe0615a29a0d3c78eb2320e0935
lsavoie11 <lsavoie11@gmail.com>
2025-06-02 15:09:41 -0500

    Lab 6 Testing Added

---
+++ module-5/Lab2-Calculator/app.test.js
@@ -0,0 +1,56 @@
+// app.test.js
+// import supertest and the express app
+const request = require("supertest");
+const app = require("./app");
+describe("Calculator Routes", () => {
+  // generate some random numbers to test the calculator
+  let number1 = Math.floor(Math.random() * 1_000_000);
+  let number2 = Math.floor(Math.random() * 1_000_000);
+  test("GET /calculator/add => answer of numbers", () => {
+    return request(app)
+      .get(`/calculator/add?num1=${number1}&num2=${number2}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          result: number1 + number2,
+        });
+      });
+  });
+
+  test("GET /calculator/subtract => answer of numbers", () => {
+    return request(app)
+      .get(`/calculator/subtract?num1=${number1}&num2=${number2}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          result: number1 - number2,
+        });
+      });
+  });
+
+  test("GET /calculator/divide => answer of numbers", () => {
+    return request(app)
+      .get(`/calculator/divide?num1=${number1}&num2=${number2}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          result: number1 / number2,
+        });
+      });
+  });
+
+  test("GET /calculator/multiply => answer of numbers", () => {
+    return request(app)
+      .get(`/calculator/multiply?num1=${number1}&num2=${number2}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          result: number1 * number2,
+        });
+      });
+  });
+});
--- module-5/Lab2-Calculator/index.js
+++ module-5/Lab2-Calculator/index.js
@@ -1,14 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 const port = 3000;
-const path = require("path");
-
-// import all calculator routes (up the top)
-const calculatorRoutes = require("./routes/calculatorRoutes");
-
-app.use(express.static(path.join(__dirname, "public")));
-// map the calculator routes to our app
-app.use("/calculator", calculatorRoutes);
 
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:${port}`);
